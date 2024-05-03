8b3c486e41a4d827230917499aec84a7343fe5a2
lukaswarren <lukaswarren728@gmail.com>
2024-05-03 13:38:31 -0600

    Changes before computer switch

--- places/models.py
+++ places/models.py
@@ -1,16 +1,42 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.contrib.auth.models import User
+
+
+class CustomUser(AbstractUser):
+    cognito_user_id = models.CharField(max_length=128, unique=True, null=True)
+
+    def __str__(self):
+        return self.username
 
 
 # Create your models here.
 class Place(models.Model):
-    city = models.CharField(max_length=30)
-    country = models.CharField(max_length=30)
+    country = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    latLng = models.CharField(max_length=50)
+    visible = models.BooleanField(default=False)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='locations')
+
+    def __str__(self):
+        return f"{self.city}, {self.country}"
+
+
+class Visit(models.Model):
+    location = models.ForeignKey(Place, related_name='visits', on_delete=models.CASCADE)
     date = models.DateField()
-    visit_number = models.IntegerField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    visitNumber = models.PositiveIntegerField()
+    author = models.CharField(max_length=100, blank=True, null=True)  # Optional author field
+    title = models.CharField(max_length=200, blank=True, null=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return f"{self.title} by {self.author} on {self.date}" if self.title and self.author else "Visit on {}".format(
+            self.date)
 
 
-class Picture(models.Model):
-    picture_id = models.CharField()
-    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+class Image(models.Model):
+    visit = models.ForeignKey(Visit, related_name='images', on_delete=models.CASCADE)
+    imgID = models.CharField(max_length=255)  # Unique identifier for the image
+
+    def __str__(self):
+        return self.imgID
--- places/serializers.py
+++ places/serializers.py
@@ -1,14 +1,35 @@
 from rest_framework import serializers
-from .models import Place, Picture
+
+from .models import Place, Visit, Image
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ['imgID']
+
+
+class VisitSerializer(serializers.ModelSerializer):
+    images = ImageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Visit
+        fields = ['date', 'visitNumber', 'author', 'title', 'content', 'images']
 
 
 class PlaceSerializer(serializers.ModelSerializer):
+    visits = VisitSerializer(many=True, read_only=True)
+
     class Meta:
         model = Place
-        fields = '__all__'
+        fields = ['country', 'city', 'latLng', 'visible', 'visits']
 
-
-class PictureSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Picture
-        fields = '__all__'
+    def create(self, validated_data):
+        visits_data = validated_data.pop('visits', [])
+        place = Place.objects.create(**validated_data)
+        for visit_data in visits_data:
+            images_data = visit_data.pop('images', [])
+            visit = Visit.objects.create(place=place, **visit_data)
+            for image_data in images_data:
+                Image.objects.create(visit=visit, **image_data)
+        return place
