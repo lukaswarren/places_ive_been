60d040b1e221c7223cc363d8bab62de74ab8beb9
lukaswarren <lukaswarren728@gmail.com>
2024-05-08 14:50:22 -0600

    User changes

--- places/models.py
+++ places/models.py
@@ -1,12 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-
-class CustomUser(AbstractUser):
-    cognito_user_id = models.CharField(max_length=128, unique=True, null=True)
-
-    def __str__(self):
-        return self.username
 
 
 # Create your models here.
@@ -15,7 +11,7 @@ class Place(models.Model):
     city = models.CharField(max_length=100)
     latLng = models.CharField(max_length=50)
     visible = models.BooleanField(default=False)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='locations')
+    user = models.ForeignKey(AbstractUser, on_delete=models.CASCADE, related_name='locations')
 
     def __str__(self):
         return f"{self.city}, {self.country}"
